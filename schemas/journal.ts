cee3012b68ab63f116452aa2b694d0dfb00db49b
Sunjay Armstead <connect@sunjayarmstead.local>
2024-02-10 21:54:06 -0500

    Add field validation and import sanity validation type

--- schemas/journal.ts
+++ schemas/journal.ts
@@ -1,17 +1,27 @@
+import type { Rule } from "sanity";
+
 export default {
   name: "journal",
   type: "document",
   title: "Journal",
   fields: [
     {
+      name: "metaDescription",
+      type: "string",
+      title: "Meta Description",
+      validation: (Rule: Rule) => Rule.required().min(140).max(150),
+    },
+    {
       name: "title",
       type: "string",
       title: "Title",
+      validation: (Rule: Rule) => Rule.required(),
     },
     {
       name: "body",
       title: "Body",
       type: "markdown",
+      validation: (Rule: Rule) => Rule.required(),
     },
   ],
 };

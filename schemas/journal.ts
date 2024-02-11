828cc9383e63ff3b3e07d947d92931ee51cd2fba
Sunjay Armstead <connect@sunjayarmstead.local>
2024-02-10 20:54:29 -0500

    Stub out Journal document type

---
+++ schemas/journal.ts
@@ -0,0 +1,12 @@
+export default {
+  name: "journal",
+  type: "document",
+  title: "Journal",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Blog Title"
+    }
+  ]
+}
